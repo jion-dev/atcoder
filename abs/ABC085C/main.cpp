57cecf2d2ae23525be7a95b6a8d0caa54dad9c98
jion-kozono <zeon66190329@gmail.com>
2022-06-21 00:59:52 +0900

    ABC085C

--- abs/ABC085C/main.cpp
+++ abs/ABC085C/main.cpp
@@ -1,7 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
 
 int main() {
-    
+  int N, Y;
+  cin >> N >> Y;
+
+  int x = -1, y = -1, z = -1;
+  for(int a = 0; a <= N; ++a) {
+    for(int b = 0; b + a <= N; ++b) {
+      int c = N - a - b;
+      int total = 10000 * a + 5000 * b + 1000 * c;
+      if(total == Y) {
+        x = a;
+        y = b;
+        z = c;
+      }
+    }
+  }
+  cout << x << " " << y << " " << z << endl;
 }
