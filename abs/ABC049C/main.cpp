509dceaf34f8179db730eaf559c0a9d128ffbec1
jion-kozono <zeon66190329@gmail.com>
2022-06-21 01:30:40 +0900

    ABC049C

--- abs/ABC049C/main.cpp
+++ abs/ABC049C/main.cpp
@@ -1,7 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
 
 int main() {
-    
+  string S;
+  cin >> S;
+
+  vector<string> accept{"dream", "dreamer", "erase", "eraser"};
+  reverse(S.begin(), S.end());
+  rep(i, 4) { reverse(accept.at(i).begin(), accept.at(i).end()); };
+
+  bool can = true;
+  for(int i = 0; i < S.size();) {
+    bool can2 = false;
+    for(int j = 0; j < 4; j++) {
+      string d = accept.at(j);
+      if(S.substr(i, d.size()) == d) {
+        can2 = true;
+        i += d.size();
+      }
+    }
+    if(!can2) {
+      can = false;
+      break;
+    }
+  }
+
+  if(can)
+    cout << "YES" << endl;
+  else
+    cout << "NO" << endl;
 }
