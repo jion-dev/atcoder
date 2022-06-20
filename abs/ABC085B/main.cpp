400cd9b73137e546fae8716da7070f237258d6d0
jion-kozono <zeon66190329@gmail.com>
2022-06-20 22:01:48 +0900

    ABC085B

--- abs/ABC085B/main.cpp
+++ abs/ABC085B/main.cpp
@@ -1,7 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+
+bool check_include(vector<int> array, int target) {
+  rep(i, array.size()) {
+    if(array.at(i) == target)
+      return true;
+  }
+  return false;
+}
 
 int main() {
-    
+  int N;
+  cin >> N;
+
+  int count = 0;
+  vector<int> uniq_array;
+  rep(i, N) {
+    int d;
+    cin >> d;
+
+    if(!check_include(uniq_array, d)) {
+      uniq_array.push_back(d);
+      count++;
+    }
+  }
+
+  cout << count << endl;
 }
