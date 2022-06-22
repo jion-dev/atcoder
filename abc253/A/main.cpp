b892e09309c1b87448b1c4c670b65ab78985e016
jion-kozono <zeon66190329@gmail.com>
2022-06-22 22:05:16 +0900

    A

--- abc253/A/main.cpp
+++ abc253/A/main.cpp
@@ -3,9 +3,12 @@ using namespace std;
 #define rep(i, n) for(int i = 0; i < (int)(n); i++)
 
 int main() {
-  int a, b, c;
-  cin >> a >> b >> c;
+  vector<int> abc(3);
+  int b;
+  cin >> abc.at(0) >> b >> abc.at(2);
+  abc.at(1) = b;
+  sort(abc.begin(), abc.end());
 
-  string answer = (a + b + c) / 3 == b ? "Yes" : "No";
+  string answer = abc.at(1) == b ? "Yes" : "No";
   cout << answer << endl;
 }
