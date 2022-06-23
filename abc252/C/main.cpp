ad8fc28dbdec501e8d64e40498140ffde709d0fb
jion-kozono <zeon66190329@gmail.com>
2022-06-23 19:35:05 +0900

    wip: C

---
+++ abc252/C/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+
+int calc(char c, int t) {
+  int index = t % 10 + 1;
+  return index - 1;
+}
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<string> s(N);
+  for(auto &nx : s) {
+    cin >> nx;
+  }
+
+  int minutes = 0;
+  bool can = false;
+  while(!can) {
+
+    rep(i, N) {}
+  }
+}
