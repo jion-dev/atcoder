493aae0c2389ffd9139030faaab46b0b56fd1e60
jion-kozono <zeon66190329@gmail.com>
2022-06-26 19:17:41 +0900

    027 use map instead of find in array

--- typical90/027/main.cpp
+++ typical90/027/main.cpp
@@ -1,7 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
 
+// これではO(N^2)なのでTLE
+// int main() {
+//   int n;
+//   cin >> n;
+//   vector<string> users(n);
+
+//   rep(i, n) {
+//     string user;
+//     cin >> user;
+//     if(*find(users.begin(), users.end(), user) != user) {
+//       users[i] = user;
+//       cout << i + 1 << endl;
+//     }
+//   }
+// }
+
+// mapを使うとO(NlogN)の計算量で、速くなる。
 int main() {
-    
+  int n;
+  cin >> n;
+  map<string, int> map_users;
+
+  rep(i, n) {
+    string user;
+    cin >> user;
+    if(map_users[user] == 0)
+      cout << i + 1 << endl;
+    map_users[user] = 1;
+  }
 }
