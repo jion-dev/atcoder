702a0f98363a5ce831ab566889f94440c97dec21
jion-kozono <zeon66190329@gmail.com>
2022-06-26 10:17:28 +0900

    004

--- typical90/004/main.cpp
+++ typical90/004/main.cpp
@@ -1,7 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
 
 int main() {
-    
+  int h, w;
+  cin >> h >> w;
+
+  vector<vector<int>> squares(h, vector<int>(w));
+  vector<int> sum_height(w);
+  vector<int> sum_width(h);
+
+  rep(i, h) {
+    rep(j, w) {
+      int square;
+      cin >> square;
+      squares[i][j] = square;
+      sum_width[i] += square;
+      sum_height[j] += square;
+    }
+  }
+
+  rep(i, h) {
+    rep(j, w) {
+      squares[i][j] = sum_width[i] + sum_height[j] - squares[i][j];
+      cout << squares[i][j];
+      if(j == w - 1) {
+        cout << endl;
+      } else {
+        cout << " ";
+      }
+    }
+  }
 }
