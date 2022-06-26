658c75b4e3e5448d631c635f3fb0ee1f052da80b
jion-kozono <zeon66190329@gmail.com>
2022-06-26 18:35:45 +0900

    024 パリティ

--- typical90/024/main.cpp
+++ typical90/024/main.cpp
@@ -1,7 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
 
 int main() {
-    
+  long long n, k;
+  cin >> n >> k;
+
+  long long as[n];
+  long long bs[n];
+
+  rep(i, n) { cin >> as[i]; }
+  rep(i, n) { cin >> bs[i]; }
+
+  long long diff = 0;
+  rep(i, n) diff += abs(as[i] - bs[i]);
+  if(diff > k) {
+    cout << "No" << endl;
+    return 0;
+  }
+
+  if(diff % 2 != k % 2) {
+    cout << "No" << endl;
+    return 0;
+  }
+
+  cout << "Yes" << endl;
+  return 0;
 }
