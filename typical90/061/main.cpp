95d05e69007734d276e4f19deb91e6fa5d907fc7
jion-kozono <zeon66190329@gmail.com>
2022-06-27 10:23:35 +0900

    061 dequeの構造

--- typical90/061/main.cpp
+++ typical90/061/main.cpp
@@ -1,7 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
 
 int main() {
-    
+  int q;
+  cin >> q;
+  deque<int> cards;
+  vector<int> answers;
+  rep(i, q) {
+    int t, x;
+    cin >> t >> x;
+    if(t == 1) {
+      cards.push_front(x);
+    }
+    if(t == 2) {
+      cards.push_back(x);
+    }
+    if(t == 3) {
+      answers.push_back(cards[x - 1]);
+    }
+  }
+  rep(i, answers.size()) { cout << answers[i] << endl; }
 }
