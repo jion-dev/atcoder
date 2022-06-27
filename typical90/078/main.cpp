3fe49163ad08b39890549c92525c35507eb8e270
jion-kozono <zeon66190329@gmail.com>
2022-06-27 22:32:24 +0900

    078 隣接リストの二次元配列

--- typical90/078/main.cpp
+++ typical90/078/main.cpp
@@ -1,7 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
 
 int main() {
-    
+  int n, m;
+  cin >> n >> m;
+  //隣接リスト
+  vector<vector<int>> g(n);
+  rep(i, m) {
+    int a, b;
+    cin >> a >> b;
+    a--;
+    b--;
+    g[a].push_back(b);
+    g[b].push_back(a);
+  };
+
+  int answer = 0;
+  rep(i, n) {
+    int cnt = 0;
+    for(int j : g[i]) {
+      if(j < i)
+        cnt++;
+    }
+    if(cnt == 1) {
+      answer++;
+    }
+  }
+  cout << answer << endl;
+  return 0;
 }
