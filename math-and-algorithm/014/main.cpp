485079c31cb8d08d76d6a422a9bf403688dedbc1
jion-kozono <zeon66190329@gmail.com>
2022-06-28 21:02:03 +0900

    014 素因数分解

--- math-and-algorithm/014/main.cpp
+++ math-and-algorithm/014/main.cpp
@@ -1,7 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
 
 int main() {
-    
+  long long N;
+  cin >> N;
+
+  // 素因数分解（空白区切りで出力）
+  bool flag = false;
+  for(long long i = 2; i * i <= N; i++) {
+    // iで破り続ける
+    while(N % i == 0) {
+      if(flag == true)
+        cout << " ";
+      flag = true;
+      N /= i;
+      cout << i;
+    }
+  }
+  //最後に残ったのは素数なので付け加える
+  if(N >= 2) {
+    if(flag == true)
+      cout << " ";
+    flag = true;
+    cout << N;
+  }
+  cout << endl;
+  return 0;
 }
