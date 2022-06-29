754508f8583eba842bf2cb233a121d66597fa070
jion-kozono <zeon66190329@gmail.com>
2022-06-29 10:37:12 +0900

    021 組み合わせnCr

--- math-and-algorithm/021/main.cpp
+++ math-and-algorithm/021/main.cpp
@@ -1,7 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
 
 int main() {
-    
+  int n, r;
+  cin >> n >> r;
+  long long top = 1;
+  long long bottom = 1;
+  rep(i, r) {
+    top *= n - i;
+    bottom *= r - i;
+  }
+  cout << top / bottom << endl;
 }
