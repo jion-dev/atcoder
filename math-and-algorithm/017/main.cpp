12ba352bcee6be2902ca267168c6678b4b13ad95
jion-kozono <zeon66190329@gmail.com>
2022-06-29 09:52:33 +0900

    017 最小公倍数

--- math-and-algorithm/017/main.cpp
+++ math-and-algorithm/017/main.cpp
@@ -1,7 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+
+ll GCD(ll x, ll y) { return (x % y) ? GCD(y, x % y) : y; }
+ll LCM(ll x, ll y) { return x / GCD(x, y) * y; }
 
 int main() {
-    
+  ll n, a1, a2;
+  cin >> n >> a1 >> a2;
+  ll ans = LCM(a1, a2);
+  n -= 2;
+  while(n--) {
+    ll a;
+    cin >> a;
+    ans = LCM(ans, a);
+  }
+
+  cout << ans << endl;
+
+  return 0;
 }
