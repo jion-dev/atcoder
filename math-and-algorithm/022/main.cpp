04870d222c1bca48d5f6f2a251ebd89954f69164
jion-kozono <zeon66190329@gmail.com>
2022-06-29 12:00:22 +0900

    022 組み合わせ

--- math-and-algorithm/022/main.cpp
+++ math-and-algorithm/022/main.cpp
@@ -1,7 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
 
 int main() {
-    
+  int S = 100000;
+  int n;
+  cin >> n;
+  int a[n];
+  vector<long long> cnt(S);
+  long long ans = 0;
+  for(int i = 1; i <= n; i++)
+    cin >> a[i];
+
+  for(int i = 1; i <= n; i++) {
+    cnt[a[i]]++;
+  }
+
+  for(int i = 1; i < S / 2; i++) {
+    ans += cnt[i] * cnt[S - i];
+  }
+  // 5000の時は例外でnCrを求める
+  ans += cnt[S / 2] * (cnt[S / 2] - 1) / 2;
+
+  cout << ans << endl;
 }
