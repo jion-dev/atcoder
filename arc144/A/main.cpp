47099f47f4f424dcf2dec3e19e09ea45c72f79b8
jion-kozono <zeon66190329@gmail.com>
2022-07-16 23:30:20 +0900

    A: 足し算の繰り上がりと各桁の和

--- arc144/A/main.cpp
+++ arc144/A/main.cpp
@@ -8,41 +8,20 @@ int main() {
   int N;
   cin >> N;
   int M = N * 2;
-  // string a = to_string(N * 2);
-  // int M = 0;
-  // rep(i, a.size()){
-  //   M += a[i] - '0';
-  // }
-  ll x = 0;
-
-  string s = "x";
-  vector<int> S;
-  S.push_back(0);
-  int currentIndex = 0;
-  int currentNum = 0;
-
-  rep(i, N) {
-    currentNum++;
-    s[currentIndex] = char(currentNum + '0');
-    S.push_back(int(s[currentIndex] - '0') + S[S.size() - 1]);
-
-    int sum = S[S.size() - 1];
-    if(sum == N && sum * 2 == M) {
-      x = stoll(s);
-      break;
-    }
-
-    if(s[currentIndex] % 4 == 0) {
-      S.clear();
-      S.push_back(0);
-      s.push_back('x');
-      currentIndex++;
-      currentNum = 0;
-    }
-  }
-
   cout << M << endl;
-  cout << x << endl;
+
+  ll x;
+  if(N - 4 < 0) {
+    x = N;
+    cout << x << endl;
+
+  } else {
+    int four_num = N / 4;
+    int rest = N % 4;
+    string s = rest == 0 ? "" : to_string(rest);
+    rep(i, four_num) { s.push_back('4'); }
+    cout << s << endl;
+  }
 
   return 0;
 }
