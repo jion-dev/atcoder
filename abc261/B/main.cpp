e9375dca0af629bc899a540af3294078c98379cb
jion-kozono <zeon66190329@gmail.com>
2022-08-07 09:21:39 +0900

    B

--- abc261/B/main.cpp
+++ abc261/B/main.cpp
@@ -2,9 +2,26 @@
 using namespace std;
 typedef long long ll;
 typedef long double ld;
-#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
 
 int main() {
-		
-		return 0;
+  int n;
+  cin >> n;
+  vector<vector<char>> d(n, vector<char>(n));
+  bool can = true;
+
+  rep(i, n) rep(j, n) cin >> d[i][j];
+
+  rep(i, n) rep(j, n) {
+    if((d[i][j] == 'W' && d[j][i] != 'L') ||
+       (d[i][j] == 'L' && d[j][i] != 'W') || (d[i][j] == 'D' && d[j][i] != 'D'))
+      can = false;
+  }
+
+  if(can)
+    cout << "correct" << endl;
+  else
+    cout << "incorrect" << endl;
+
+  return 0;
 }
