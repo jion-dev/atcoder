538838a4d66c8db4b0c097722d63f0920d459044
jion-kozono <zeon66190329@gmail.com>
2022-08-07 09:07:05 +0900

    A: 共通区間の長さ

--- abc261/A/main.cpp
+++ abc261/A/main.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 typedef long long ll;
 typedef long double ld;
-#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
 
 int main() {
-		
-		return 0;
+  int l1, r1, l2, r2;
+  cin >> l1 >> r1 >> l2 >> r2;
+
+  cout << max(0, min(r1, r2) - max(l1, l2)) << endl;
+  return 0;
 }
