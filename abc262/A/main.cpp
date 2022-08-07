9d8ea9909211e82cc586c8bd037306e8cc2fdabe
jion-kozono <zeon66190329@gmail.com>
2022-08-07 13:33:03 +0900

    A

--- abc262/A/main.cpp
+++ abc262/A/main.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 typedef long long ll;
 typedef long double ld;
-#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
 
 int main() {
-		
-		return 0;
+  int y;
+  cin >> y;
+  int rest = y % 4;
+  int ans = rest == 0 ? y + 2 : rest == 1 ? y + 1 : rest == 3 ? y + 3 : y;
+  cout << ans << endl;
+  return 0;
 }
