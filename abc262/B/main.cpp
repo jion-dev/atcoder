e0b2e5f06d85445898b26e14ff299c0dc8d73991
jion-kozono <zeon66190329@gmail.com>
2022-08-07 09:25:48 +0900

    wip: abc262

---
+++ abc262/B/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+
+int main() {
+		
+		return 0;
+}
