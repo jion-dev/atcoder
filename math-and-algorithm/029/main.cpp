648839de561b8011df8a3723ff088fb7a2d2993b
jion-kozono <zeon66190329@gmail.com>
2022-08-08 08:30:55 +0900

    029: 動的計画法 階段上り方

--- math-and-algorithm/029/main.cpp
+++ math-and-algorithm/029/main.cpp
@@ -1,7 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
 
 int main() {
-    
+  int n, dp[54];
+  cin >> n;
+  for(int i = 0; i <= n; i++) {
+    if(i <= 1) {
+      dp[i] = 1;
+    } else {
+      dp[i] = dp[i - 1] + dp[i - 2];
+    }
+  }
+  cout << dp[n] << endl;
 }
