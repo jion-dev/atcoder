f7340cc912a6e5205a8b98bf36ee8e78fa9c2eab
jion-kozono <zeon66190329@gmail.com>
2022-08-11 09:36:01 +0900

    A: DP足場

---
+++ dp/A/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+
+int N, h[10009], dp[10009];
+int main() {
+  cin >> N;
+  for(int i = 1; i <= N; i++)
+    cin >> h[i];
+
+  dp[0] = 0;
+  dp[1] = 0;
+  for(int i = 2; i <= N; i++) {
+    if(i == 2) {
+      dp[i] = abs(h[i] - h[i - 1]);
+    } else {
+      int v1 = dp[i - 1] + abs(h[i] - h[i - 1]);
+      int v2 = dp[i - 2] + abs(h[i] - h[i - 2]);
+      dp[i] = min(v1, v2);
+    }
+  }
+
+  cout << dp[N] << endl;
+
+  return 0;
+}
