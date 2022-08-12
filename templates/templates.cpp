4dae9b1034dec9ed3e1c736f592cbfe7cafcd3a3
jion-kozono <zeon66190329@gmail.com>
2022-08-12 22:45:42 +0900

    fix template

--- templates/templates.cpp
+++ templates/templates.cpp
@@ -5,7 +5,7 @@ typedef long double ld;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define rep3(i, m, n) for (int i = (m); i < (int)(n); i++)
 #define ALL(x) std::begin(x), std::end(x)
-#define debug()  do{view(var);}while(0)
+#define debug(true)  do{view(var);}while(0)
 #define debugN(var)  do{std::cout << #var << " : ";view(var);}while(0)
 template<typename T> void view(T e){std::cout << e << std::endl;}
 template<typename T> void view(const std::vector<T>& v){for(const auto& e : v){ std::cout << e << " "; } std::cout << std::endl;}
