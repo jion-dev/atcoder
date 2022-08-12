674fc6abac48d4d3e3540509da25e8ffcc061f5a
jion-kozono <zeon66190329@gmail.com>
2022-08-12 13:42:24 +0900

    fix template

--- templates/templates.cpp
+++ templates/templates.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 typedef long long ll;
 typedef long double ld;
-#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep3(i, m, n) for (int i = (m); i < (int)(n); i++)
+#define ALL(x) std::begin(x), std::end(x)
+
 
 int main() {
-  
+
   return 0;
 }
