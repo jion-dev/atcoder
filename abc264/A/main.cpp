3d43c82b7b5bbd8c25e7048149f352811908d62e
jion-kozono <zeon66190329@gmail.com>
2022-08-13 22:42:45 +0900

    abc264 A

---
+++ abc264/A/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define rep3(i, m, n) for(int i = (m); i < (int)(n); i++)
+#define ALL(x) x.begin(), x.end()
+#define debug(true)                                                            \
+  do {                                                                         \
+    view(var);                                                                 \
+  } while(0)
+#define debugN(var)                                                            \
+  do {                                                                         \
+    std::cout << #var << " : ";                                                \
+    view(var);                                                                 \
+  } while(0)
+template <typename T> void view(T e) { std::cout << e << std::endl; }
+template <typename T> void view(const std::vector<T> &v) {
+  for(const auto &e : v) {
+    std::cout << e << " ";
+  }
+  std::cout << std::endl;
+}
+template <typename T> void view(const std::vector<std::vector<T>> &vv) {
+  for(const auto &v : vv) {
+    view(v);
+  }
+}
+
+int main() {
+  int L, R;
+  cin >> L >> R;
+  L--;
+  R--;
+
+  string s = "atcoder";
+  rep3(i, L, R + 1) { cout << s[i]; }
+  cout << endl;
+  return 0;
+}
