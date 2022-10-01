12491824f7d8e4b0be430c62c11710da7ca2dc38
jion-kozono <zeon66190329@gmail.com>
2022-10-01 22:41:20 +0900

    abc271/B

---
+++ abc271/B/main.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define rep3(i, m, n) for(int i = (m); i < (int)(n); i++)
+#define ALL(x) x.begin(), x.end()
+#define debug(var)                                                             \
+  do {                                                                         \
+    view(var);                                                                 \
+  } while(0)
+#define debugN(var)                                                            \
+  do {                                                                         \
+    std::cout << #var << " : ";                                                \
+    view(var);                                                                 \
+  } while(0)
+template <typename T> void view(T e) { std::cout << e << std::endl; }
+template <typename T> void view(const std::vector<T> &v) {
+  for(const auto &e : v) {
+    std::cout << e << " ";
+  }
+  std::cout << std::endl;
+}
+template <typename T> void view(const std::vector<std::vector<T>> &vv) {
+  for(const auto &v : vv) {
+    view(v);
+  }
+}
+
+int main() {
+  ll N, Q;
+  cin >> N >> Q;
+  vector<vector<ll>> L(N);
+  rep(i, N) {
+    ll l;
+    cin >> l;
+    rep(j, l) {
+      int a;
+      cin >> a;
+      L[i].push_back(a);
+    }
+  }
+
+  rep(i, Q) {
+    int s, t;
+    cin >> s >> t;
+    s--, t--;
+    debug(L[s][t]);
+  }
+
+  return 0;
+}
