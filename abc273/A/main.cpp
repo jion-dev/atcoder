85f179afa667bbd5a9684482c6533ccd3a0d6057
jion-kozono <zeon66190329@gmail.com>
2022-10-15 22:28:11 +0900

    abc273/A

---
+++ abc273/A/main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define rep3(i, m, n) for(int i = (m); i < (int)(n); i++)
+#define ALL(x) x.begin(), x.end()
+#define debug(var)                                                             \
+  do {                                                                         \
+    view(var);                                                                 \
+  } while(0)
+#define debugN(var)                                                            \
+  do {                                                                         \
+    std::cout << #var << " : ";                                                \
+    view(var);                                                                 \
+  } while(0)
+template <typename T> void view(T e) { std::cout << e << std::endl; }
+template <typename T> void view(const std::vector<T> &v) {
+  for(const auto &e : v) {
+    std::cout << e << " ";
+  }
+  std::cout << std::endl;
+}
+template <typename T> void view(const std::vector<std::vector<T>> &vv) {
+  for(const auto &v : vv) {
+    view(v);
+  }
+}
+
+int f(int k) {
+  if(k == 0)
+    return 1;
+  return k * f(k - 1);
+}
+
+int main() {
+  int N;
+  cin >> N;
+
+  if(N == 0) {
+    debug(1);
+  } else {
+    debug(f(N));
+  }
+  return 0;
+}
