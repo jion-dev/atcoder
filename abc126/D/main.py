e83141d87755e9c09341627830d9798cc33d61ff
jion-kozono <zeon66190329@gmail.com>
2023-03-18 20:41:10 +0900

    abc126

--- abc126/D/main.py
+++ abc126/D/main.py
@@ -4,3 +4,5 @@ def I(): return int(sys.stdin.readline().rstrip())
 def LI(): return list(map(int,sys.stdin.readline().rstrip().split()))
 def S(): return sys.stdin.readline().rstrip()
 def LS(): return list(sys.stdin.readline().rstrip().split())
+N = I()
+A = [LI() for _ in range(N)]
