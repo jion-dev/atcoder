41d762aa2bffa1710e8bfc2481148f7f717f2dbd
jion_kozono <zeon66190329@gmail.com>
2023-09-03 20:05:31 +0900

    clear

--- abc318/B/main.py
+++ abc318/B/main.py
@@ -9,38 +9,18 @@ def MS(): return input().split()
 def LS(): return list(input().split())
 
 N = int(input())
+g=[[False for _ in range(100)]for _ in range(100)]
 
-# 面積Sを初期化
-S = 0
+for k in range(N):
+    a,b,c,d=MI()
+    for i in range(a,b):
+        for j in range(c,d):
+            g[i][j]=True
 
-# 各シートの座標範囲をリストに格納
-sheets = []
-for _ in range(N):
-    A, B, C, D = map(int, input().split())
-    sheets.append((A, B, C, D))
+ans = 0
+for i in range(100):
+    for j in range(100):
+        if g[i][j]:
+            ans+=1
 
-# 全てのシートの面積を計算
-for i in range(N):
-    width = sheets[i][1] - sheets[i][0]
-    height = sheets[i][3] - sheets[i][2]
-    S += width * height
-
-# 重複している部分の面積を引く
-for i in range(N):
-    for j in range(i + 1, N):
-        overlap_x = max(0, min(sheets[i][1], sheets[j][1]) - max(sheets[i][0], sheets[j][0]))
-        overlap_y = max(0, min(sheets[i][3], sheets[j][3]) - max(sheets[i][2], sheets[j][2]))
-        overlap_area = overlap_x * overlap_y
-        S -= overlap_area
-
-# 全てのシートが重なる部分の面積を足す
-for i in range(N):
-    for j in range(i + 1, N):
-        for k in range(j + 1, N):
-            overlap_x = max(0, min(sheets[i][1], min(sheets[j][1], sheets[k][1])) - max(sheets[i][0], max(sheets[j][0], sheets[k][0])))
-            overlap_y = max(0, min(sheets[i][3], min(sheets[j][3], sheets[k][3])) - max(sheets[i][2], max(sheets[j][2], sheets[k][2])))
-            overlap_area = overlap_x * overlap_y
-            S += overlap_area
-
-# Sを整数形式で出力
-print(S)
+print(ans)
