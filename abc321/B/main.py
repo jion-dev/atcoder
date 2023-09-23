078794a1cfdcba8129c21529eeaa64a865780d6a
jion_kozono <zeon66190329@gmail.com>
2023-09-24 06:56:14 +0900

    abc321/B

--- abc321/B/main.py
+++ abc321/B/main.py
@@ -8,25 +8,18 @@ def S(): return input()
 def MS(): return input().split()
 def LS(): return list(input().split())
 
-def calculate_score(N, X, A):
-    min_s = min(A)
-    max_s = max(A)
-    sum_A = sum(A)
-    sum_A_except_min_mum = sum_A - min_s-max_s
-    if sum_A_except_min_mum > 100:
-      total_s = X - sum_A_except_min_mum
-      return total_s
-    else:
-        if max_s >= X:
-          sum_A -= max_s
-          return X - sum_A
-        else:
-          return -1
+n, x = MI()
+A = LI()
+A.append(0)
 
-# 入力を受け取る
-N, X = map(int, input().split())
-A = list(map(int, input().split()))
+while (A[-1] <= 100):
+  b = A
+  b = sorted(A)
+  s = 0
+  for i in range(1, n-1): s+=b[i]
+  if s >= x:
+    print(A[-1])
+    exit(0)
+  A[-1] += 1
 
-# 最小スコアを計算して出力
-result = calculate_score(N, X, A)
-print(result)
+print(-1)
