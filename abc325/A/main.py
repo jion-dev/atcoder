1658320b19a4b948f7a835f0f88f9ffc8e3b0949
jion_kozono <zeon66190329@gmail.com>
2023-10-21 22:25:01 +0900

    abc325/A

---
+++ abc325/A/main.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+import bisect,collections,copy,heapq,itertools,math,string,queue
+import sys
+def I(): return int(input())
+def MI(): return map(int,input().split())
+def LI(): return list(map(int,input().split()))
+def S(): return input()
+def MS(): return input().split()
+def LS(): return list(input().split())
+
+s, t = MS()
+
+print(s + " " + "san")
