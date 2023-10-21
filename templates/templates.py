2c9c4a39c7acf4bf8f0dacdad862d7f89a3eb4af
jion_kozono <zeon66190329@gmail.com>
2023-10-22 08:34:45 +0900

    fix: templates.py

--- templates/templates.py
+++ templates/templates.py
@@ -9,4 +9,4 @@ def MS(): return input().split()
 def LS(): return list(input().split())
 dx = [-1, 1, 0, 0, -1, -1, 1, 1]
 dy = [0, 0, -1, 1, -1, 1, -1, 1]
-def is_valid(x, y, H, W): return 0 <= x < H and 0 <= y < W
+def is_d_valid(x, y, H, W): return 0 <= x < H and 0 <= y < W
