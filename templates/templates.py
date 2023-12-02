db152aa9b865cb837f8b8d2fc824d67b8ea67521
jion_kozono <zeon66190329@gmail.com>
2023-12-02 21:13:10 +0900

    fix: template

--- templates/templates.py
+++ templates/templates.py
@@ -1,9 +1,16 @@
 #! /usr/bin/env python
-import bisect,collections,copy,heapq,itertools,math,string,queue
+import bisect
+import collections
+import copy
+import heapq
+import itertools
+import math
+import string
+import queue
 import sys
 def I(): return int(input())
-def MI(): return map(int,input().split())
-def LI(): return list(map(int,input().split()))
-def S(): return input()
+def MI(): return map(int, input().split())
+def LI(): return list(map(int, input().split()))
+def St(): return input()
 def MS(): return input().split()
 def LS(): return list(input().split())
