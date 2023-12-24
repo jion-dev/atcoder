ee902868b48c0116aaf5a4b8e6a6cdfe892261d2
jion_kozono <zeon66190329@gmail.com>
2023-12-24 11:53:54 +0900

    answer: abc334/B

--- abc334/B/main.py
+++ abc334/B/main.py
@@ -16,17 +16,22 @@ def MS(): return input().split()
 def LS(): return list(input().split())
 
 
-A, M, L, R = MI()
+# answer
+a, m, l, r = map(int, input().split())
+l -= a
+r -= a
+print(r // m - (l - 1) // m)
+
+# mine
+# A, M, L, R = MI()
+# def count_christmas_trees(A, M, L, R):
+#     first_tree_after_L = A + ((L - A + M - 1) // M) * M
+
+#     if first_tree_after_L > R:
+#         print(0)
+#         return
+
+#     print((R - first_tree_after_L) // M + 1)
 
 
-def count_christmas_trees(A, M, L, R):
-    first_tree_after_L = A + ((L - A + M - 1) // M) * M
-
-    if first_tree_after_L > R:
-        print(0)
-        return
-
-    print((R - first_tree_after_L) // M + 1)
-
-
-count_christmas_trees(A, M, L, R)
+# count_christmas_trees(A, M, L, R)
